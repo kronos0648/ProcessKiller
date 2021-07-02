ef194032c2b44c8c86b787d38551c17b2423c5ec
kronos0648 <dkstjdgus15@gmail.com>
2021-07-02 14:59:57 +0900

    Server Program of Process Killer for Window

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ProcessKiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server=new Server();
+        }
+    }
+}
